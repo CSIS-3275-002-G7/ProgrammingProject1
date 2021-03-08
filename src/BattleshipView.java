181a4387f32ce041661613c034fe43888c7b5b44
relovag <relovag@student.douglascollege.ca>
2021-03-08 12:44:26 -0800

    Add guess limit feature

--- src/BattleshipView.java
+++ src/BattleshipView.java
@@ -3,12 +3,25 @@ import javax.swing.*;
 public class BattleshipView {
     JLayeredPane layeredPane;
     JTextArea messageLabel;
+    JTextArea guessCounter;
+    JTextField guessInput;
     ImageIcon ship = new ImageIcon("src/ship.png");
     ImageIcon miss = new ImageIcon("src/miss.png");
 
-    public BattleshipView(JLayeredPane layeredPane, JTextArea messageLabel) {
+    public BattleshipView(JLayeredPane layeredPane, JTextArea messageLabel,
+                          JTextArea guessCounter, JTextField guessInput) {
         this.layeredPane = layeredPane;
         this.messageLabel = messageLabel;
+        this.guessCounter = guessCounter;
+        this.guessInput = guessInput;
+    }
+
+    public JLayeredPane getLayeredPane() {
+        return layeredPane;
+    }
+
+    public JTextField getGuessInput() {
+        return guessInput;
     }
 
     public void displayMessage(String msg) {
@@ -29,5 +42,9 @@ public class BattleshipView {
         int col = guess.charAt(1) - '0';
         missLabel.setBounds(170 +(95 * col), 95 + (95 * row), 100, 100);
         this.layeredPane.add(missLabel, 0);
+    }
+
+    public void updateGuesses(int guessesLeft) {
+        this.guessCounter.setText("Guesses Remaining: " + guessesLeft);
     }
 }
